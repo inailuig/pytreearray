d4d574209364b45bec31b5a8d169b16f27b85806
Clemens Giuliani <clemens@inailuig.it>
2021-05-27 15:51:58 +0200

    requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
-# TODO requirements
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="pytreearray",
     version="0.0.1",
     packages=find_packages(),
+    install_requires=requirements,
 )
