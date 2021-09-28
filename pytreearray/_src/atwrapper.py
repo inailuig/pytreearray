9270f939b0fdc801499582a963a4651583df246d
Clemens Giuliani <clemens@inailuig.it>
2021-09-28 19:34:10 +0200

    wip

--- pytreearray/_src/atwrapper.py
+++ pytreearray/_src/atwrapper.py
@@ -1,7 +1,8 @@
 from flax import struct
 from numbers import Number
 
-from .core import PyTreeArray, PyTreeArray1
+from .core import PyTreeArray, _arr_treedef
+from .util import tree_allclose
 
 import jax
 
@@ -11,6 +12,18 @@ class _IndexUpdateHelper:
     pytreearr: PyTreeArray
 
     def __getitem__(self, indices):
+        # only support indexing the first array for now
+        # for multidimensional arrays in the treedefs[0] do
+        # x[(1,1,1),]
+        #
+        # TODO multiple indices & not just arrays, e.g. x[(1,1), (2,3), 'a'], & partial indexing of those
+
+        assert self.treedefs[0] == _arr_treedef
+        # TODO support slice & ellipsis
+        # TODO vectors with scalars?
+        if isinstance(indices, int):
+            indices = (indices,)
+        assert self.axes[0] >= len(indices)
         return _IndexUpdateRef(self.pytreearr, indices)
 
 
@@ -19,7 +32,43 @@ class _IndexUpdateRef:
     pytreearr: PyTreeArray
     index: "idx"
 
+    def _index_full_dim(self):
+        return self.pytreearr.axes[0] == len(self.indices)
+
+    def _new_treedefs(self):
+        if self._index_full_dim():
+            return self.axes[1:]
+        else:
+            return (self.axes[0] - len(indices),) + self.axes[1:]
+
+    def _new_axes(self):
+        if self._index_full_dim():
+            return self.treedefs[1:]
+        else:
+            return self.treedefs
+
+    def _check(self, val):
+        if isinstance(val, PyTreeArray):
+
+            def _equal(xy):
+                x, y = xy
+                return x == y
+
+            assert all(map(_equal, zip(self._new_treedefs(), val.treedefs)))
+            assert tree_allclose(val.axes, self._new_axes())
+        else:
+            if len(self._new_treedefs()) == 0:
+                raise NotImplementedError
+            elif len(self._new_treedefs()) == 1:
+                assert jax.tree_structure(val) == self._new_treedefs()[0]
+                assert tree_allclose(jax.tree_map(jnp.ndim, val), self._new_axes()[0])
+
+            else:
+                raise NotImplementedError
+
     def set(self, val):
+
+        self._check(val)
 
         if isinstance(val, Number) or hasattr(val, "shape") and val.ndim == 0:
 
@@ -34,12 +83,14 @@ class _IndexUpdateRef:
                 return x.at[self.index].set(val)
 
             _val = val if not isinstance(val, PyTreeArray) else val.tree
-            _tree = jax.tree_multimap(_update, self.pytreearr.tree, _val)
-            return PyTreeArray(_tree, self.pytreearr.treedefs, self.pytreearr.axes)
+            return jax.tree_multimap(_update, self.pytreearr, _val)
 
     def get(self):
         def _get(x):
             return x.at[self.index]
 
         tree = jax.tree_map(_get, self.pytreearr.tree)
-        return PyTreeArray1(tree)
+        if self.pytreearr.axes[0] == len(self.indices):
+            return PyTreeArray(tree, self.treedefs[1:], self.axes[1:])
+        else:
+            return PyTreeArray(tree, self.treedefs, (self.axes[0] - len(indices),) + self.axes[1:])
